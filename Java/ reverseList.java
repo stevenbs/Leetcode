9f4dd8c04beadaccac4d71e22c691ae4045b3305
steve <zhan2661@umn.edu>
2017-12-23 22:17:27 -0600

    Add files via upload

---
+++ Java/ reverseList.java
@@ -0,0 +1,14 @@
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode pre = null;
+        ListNode cur = head;
+        ListNode next = null;
+        while(cur!=null){
+            next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+         return pre;  
+    }
+}
