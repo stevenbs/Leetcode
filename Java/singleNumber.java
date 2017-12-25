26a69f91e5e4e76112f18035538e9a5d4be37064
steve <zhan2661@umn.edu>
2017-12-24 18:40:42 -0600

    Update singleNumber.java

--- Java/singleNumber.java
+++ Java/singleNumber.java
@@ -1,5 +1,6 @@
 class Solution {
     public int singleNumber(int[] nums) {
+        1):
         Set<Integer> str = new HashSet <Integer>();
         for(int i=0;i<nums.length;i++){
             if(str.contains(nums[i])){
@@ -15,6 +16,22 @@ class Solution {
             result = it.next();
             }
             return result;
-        
+        2):
+        Map<Integer,Integer> map = new HashMap<>();
+            for(int i=0;i<nums.length;i++){
+                if(map.containsKey(nums[i])){
+                    map.replace(nums[i],1);
+                }else{
+                    map.put(nums[i],0);
+                }
+            }
+                int result =0;
+                for(Map.Entry<Integer,Integer> entry : map.entrySet()){
+                    if(entry.getValue()==0){
+                        result= entry.getKey();
+                    }
+                }
+            return result;
     }
+    
 }
