1aeb3377fec73b26cc1065c2ad1e0b8d709d0fdb
steve <zhan2661@umn.edu>
2017-12-28 20:07:17 -0600

    Update fizzBuzz.java

--- Java/fizzBuzz.java
+++ Java/fizzBuzz.java
@@ -4,7 +4,7 @@ class fizzBuzz{
         List<String> result = new ArrayList<String>();
         for(int i=1;i<=n;i++){
               if(i%3 ==0 && i%5==0){
-                result.add("FizzBuzz");
+                result.add("FizzBuzz");// just check the number could %3 or %5 or %3 and %5.
               }else if(i%3 ==0){
                 result.add("Fizz");
               }else if(i%5==0){
