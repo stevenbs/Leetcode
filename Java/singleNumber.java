fcdfea159359f20ed1438a2559dbbd39442649f7
steve <zhan2661@umn.edu>
2017-12-28 20:12:12 -0600

    Update singleNumber.java

--- Java/singleNumber.java
+++ Java/singleNumber.java
@@ -3,7 +3,7 @@ class Solution {
         1):
         Set<Integer> str = new HashSet <Integer>();
         for(int i=0;i<nums.length;i++){
-            if(str.contains(nums[i])){
+            if(str.contains(nums[i])){//use Hashset to remove the appears twich items.
                 str.remove(nums[i]);
             }
             else{
@@ -22,12 +22,12 @@ class Solution {
                 if(map.containsKey(nums[i])){
                     map.replace(nums[i],1);
                 }else{
-                    map.put(nums[i],0);
+                    map.put(nums[i],0);//use the Hashmap to count appears one is 0; twice or more is 1;
                 }
             }
                 int result =0;
                 for(Map.Entry<Integer,Integer> entry : map.entrySet()){
-                    if(entry.getValue()==0){
+                    if(entry.getValue()==0){ // find the value is 0;
                         result= entry.getKey();
                     }
                 }
