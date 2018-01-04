5735b7d2c33fa7f1ff2b9469a6b2763f9d20c829
steve <zhan2661@umn.edu>
2018-01-03 18:55:05 -0600

    Add files via upload

---
+++ Java/Find the Duplicate Number.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            if(map.containsKey(nums[i])){
+                map.put(nums[i],map.get(nums[i])+1);
+            }else{
+                map.put(nums[i],1);
+            }
+        }
+        int n =0;
+        for(Integer key : map.keySet()){
+            if(map.get(key)>1){
+                n=key;
+                break;
+            }
+        }
+        return n;
+    }
+}
